ab45812c8c7df6af2d208a155957222f36bd879f
fahadshoukat <fraza6970@gmail.com>
2022-04-26 21:53:01 +0500

    useRef hook with MutableRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import "./App.css";
-import DomRef from "./components/ref/DomRef";
+import MutableRef from "./components/ref/MutableRef";
 
 function App() {
   return <div className="App">
-    <DomRef />
+    <MutableRef />
   </div>;
 }
 
